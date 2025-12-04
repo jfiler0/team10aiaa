71fe481f1c5bc87567325671634af4760894da19
jfiler0 <jfiler@vt.edu>
2025-12-04 16:43:24 -0500

    Fixed WE MTOW mix

--- Sizing/concept_control.m
+++ Sizing/concept_control.m
@@ -24,7 +24,7 @@
 
 % Controls
     % Chose Your Concept
-    CN = 8; % COLUMN NUMBER
+    CN = 1; % COLUMN NUMBER
         % 1 -> F18E
         % 2 -> F18E_Sized (for testing)
         % 3 -> F16
@@ -48,7 +48,7 @@
 
     skip_max_ranges = true; % This can take a bit of time so if you are exploring other parameters consider just disabling it
 
-    write_to_xlsx = false; % Toggle actual writing to the excel file (for debugging)
+    write_to_xlsx = true; % Toggle actual writing to the excel file (for debugging)
 
 %% Initlization Functions
     build_atmosphere_lookup(-5000, ft2m(100000), 500); % Refresh atmosphere lookup
@@ -130,7 +130,7 @@
     fixed_input.KLOC = readVar('KLOC', CN, T); % in kilo-lines of code
     fixed_input.fold_ratio = readVar('Fold Ratio', CN, T);
     
-    geom.empty_weight = lb2N(readVar('Empty Weight [lb]', CN, T)); % Gotta be Newtons m8. This drives MTOW using historical relations which eventually informs the amount of fuel which can be carried
+    geom.mtow = lb2N(readVar('MTOW [lb]', CN, T)); % Gotta be Newtons m8. This drives MTOW using historical relations which eventually informs the amount of fuel which can be carried
     geom.W_F = lb2N(readVar('Fixed Weight [lb]', CN, T)); % N - Fixed Weight (Avionics)
     geom.span = readVar('Wing Span [m]', CN, T); % m - Wing Span
     geom.Lambda_LE = readVar('LE Sweep [deg]', CN, T); % deg - Leading Edge Sweep
@@ -164,8 +164,8 @@
 
 %% Make the plane object
     disp("Building plane object...")
-    %                                     empty_weight,       Lambda_LE,     c_r,       c_t,    span,        num_engine,      engine,      W_F
-    plane = planeObj(fixed_input, tail_input, name, geom.empty_weight, geom.Lambda_LE, geom.c_r, geom.c_t, geom.span,  geom.num_engine, geom.engine, geom.W_F);
+    %                                                  mtow,       Lambda_LE,     c_r,       c_t,    span,        num_engine,      engine,      W_F
+    plane = planeObj(fixed_input, tail_input, name, geom.mtow, geom.Lambda_LE, geom.c_r, geom.c_t, geom.span,  geom.num_engine, geom.engine, geom.W_F);
     plane = plane.applyLoadout(clean_loadout); % Just two sidewinders
 
 %% Size The Plane (Optional)
@@ -238,7 +238,7 @@ end
 %% Assign Derived Aircraft Geometry
     disp("Writing Derived Geometry...")
 
-    T = assignVar(N2lb(plane.MTOW), 'MTOW [lb]', CN, T);
+    T = assignVar(N2lb(plane.WE), 'Empty Weight [lb]', CN, T);
     
     plane = plane.applyLoadout(strike_loadout); % Should have the highest payload weight. Technically can reduce by moving to wing tanks
     internal_fuel = plane.max_fuel_weight;
@@ -250,9 +250,11 @@ end
     T = assignVar(plane.c_avg, 'Average Chord [m]', CN, T);
     T = assignVar(plane.Lambda_TE, 'TE Sweep [deg]', CN, T);
     T = assignVar(plane.S_wing, 'Wing Area [m2]', CN, T);
-    % T = assignVar( m2ft(plane.span * plane.fixed_input.fold_ratio), 'Folded Span [ft]', CN, T);
+    
     T = assignVar(plane.l_opt, 'Optimum Tail Arm [m]', CN, T);
     T = assignVar(plane.S_h, 'Hor Stab Planform Area [m2]', CN, T);
+    T = assignVar( m2ft(plane.fold_span), 'Folded Span [ft]', CN, T);
+    T = assignVar( m2ft(plane.fold_height), 'Folded Height [ft]', CN, T);
     
 
 

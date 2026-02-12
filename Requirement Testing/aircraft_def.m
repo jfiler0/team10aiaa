2f381eac3c99334c2212acad5a08e6f7c5b2aa80
jfiler0 <jfiler@vt.edu>
2026-02-12 14:26:37 -0500

    Removing the old RequrirementsTesting folder/project

--- Requirement Testing/aircraft_def.m
+++
@@ -1,7 +0,0 @@
-function ac = aircraft_def()
-    ac.Name = "ConceptA";
-    ac.Span = 18.5;   % meters
-    ac.WingArea = 45; % m^2
-    ac.StabilityMargin = 0.08; % placeholder
-    save('aircraft_data.mat','ac')
-end

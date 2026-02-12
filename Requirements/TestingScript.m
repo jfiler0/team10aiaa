fb07c67b59d662c66aad20161e0faefe9da43552
jfiler0 <jfiler@vt.edu>
2026-02-12 14:18:44 -0500

    Added Requirements folder and project to satsify the "Requirements" asignment

---
+++ Requirements/TestingScript.m
@@ -0,0 +1,42 @@
+classdef TestingScript < matlab.unittest.TestCase
+
+    methods (TestClassSetup)
+
+    end
+
+    methods (Test)
+        function verifyWingUnfoldedSpan(testCase)
+            span_val = getComposerVal("MainWing", "Span");
+            testCase.verifyLessThan(span_val, ft2m(60));
+        end
+        function verifyWingFoldedSpan(testCase)
+            span_val = getComposerVal("MainWing", "Span");
+            fold_ratio_val = getComposerVal("MainWing", "FoldRatio");
+            testCase.verifyLessThan(span_val * (1 - fold_ratio_val), ft2m(35));
+        end
+        function verifyAircraftHeight(testCase)
+            span_val = getComposerVal("MainWing", "Span");
+            fold_ratio_val = getComposerVal("MainWing", "FoldRatio");
+            testCase.verifyLessThan( 1 + span_val * fold_ratio_val, ft2m(18.5));
+        end
+        function verifyAircraftLength(testCase)
+            length_val = getComposerVal("Fuselage", "Length");
+            testCase.verifyLessThan( length_val, ft2m(50));
+        end
+        function verifyNumEngines(testCase)
+            num_engines = getComposerVal("Fuselage/F100 Engines", "NumEngines");
+            testCase.verifyTrue( (num_engines == 1) || (num_engines == 2) );
+        end
+        function verifyDesignLoadFactor(testCase)
+            design_load_factor = getComposerVal("Fuselage", "DesignLoadFactor");
+            testCase.verifyGreaterThanOrEqual(design_load_factor, 7);
+        end
+    end
+end
+
+function val = getComposerVal(comp_path, parameter_name)
+    model = systemcomposer.openModel("AircraftArch");
+    comp = lookup(model, Path="AircraftArch/" + comp_path);
+    comp_param = comp.getParameter(parameter_name);
+    val = sscanf(comp_param.Value, '%f', 1);
+end

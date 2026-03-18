5624b4f0100be16f96502a5d31ee53394ea18a24
jfiler0 <jfiler@vt.edu>
2026-03-18 15:57:48 -0400

    Bunch of bug improvements: listAircraft had an extra space Added room for roscam and nicolai weight codes Made it so component array was tracked Ensured engine lookup string was captialized as inputs are forced lower case

--- SizingCodeV2/Helpers/ProcessGeometry/processGeometryWeight.m
+++ SizingCodeV2/Helpers/ProcessGeometry/processGeometryWeight.m
@@ -5,6 +5,8 @@ function geom = processGeometryWeight(geom, settings)
         case settings.codes.WE_RAYMER
             WE = geom.weights.mtow.v * geom.weights.raymer.A.v * N2lb(geom.weights.mtow.v)^geom.weights.raymer.C.v;
     
+            weight_comps = struct(); % empty
+
         case settings.codes.WE_COMPS
             weight_comps = getRaymerWeightStruct(geom);
             fn = fieldnames(weight_comps);  % get all field names
@@ -13,17 +15,38 @@ function geom = processGeometryWeight(geom, settings)
                 total = total + weight_comps.(fn{i});
             end
             WE = total;
+            
+        case settings.codes.WE_Nicolai
+            settings = readSettings();
+            model = model_class(settings, geom);
+            perf = performance_class(model);
+
+            % iterate here
+
+            weight_comps = struct(); % set this to your list of components
+            WE = 1000; % Need to make this something
+
+        case settings.codes.WE_Roskam
+            settings = readSettings();
+            model = model_class(settings, geom);
+            perf = performance_class(model);
+
+            % iterate here
+
+            weight_comps = struct(); % set this to your list of components
+            WE = 1000; % Need to make this something
+
     end
 
     % Usally, we enter derived equations as a string. This can be fixed later. Easier to just used derived override here
     geom.weights.empty = json_entry("Empty Weight", WE, "N", geom, true);
     geom.weights.max_fuel_weight = json_entry("Max Fuel Weight", settings.WF_max_ratio * (geom.weights.mtow.v - geom.weights.empty.v), "N", geom, true);
-    geom.weight.loaded = json_entry("Loaded Weight", 0, "N", geom, true);
+    geom.weights.loaded = json_entry("Loaded Weight", 0, "N", geom, true);
+    geom.weights.components = weight_comps;
     
     % TODO: How to compute usable fuel weight
 
     % obj.max_fuel_weight = obj.MTOW - obj.WE - obj.W_P - obj.W_Tanks - obj.W_F;
     % obj.internal_fuel_weight = 0.7 * obj.max_fuel_weight; % Accounts for tanks in an actual mission
     % obj.mid_mission_weight = obj.MTOW - obj.max_fuel_weight / 2; % Assume half of fuel is burned
-
 end

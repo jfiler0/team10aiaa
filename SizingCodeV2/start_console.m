e89d9687d564568c8730f91f1b30a7d7211bc26a
xandercfry <xandercfry@vt.edu>
2026-03-19 18:10:47 -0400

    no change

--- SizingCodeV2/start_console.m
+++ SizingCodeV2/start_console.m
@@ -8,7 +8,8 @@ console = console_class();
 % cond order: H, MV, N, W, T
 
 commandList = ["load f18_superhornet"];
-% commandList = ["load kevin_cad","INSPECT","GRAPHING", "maxPerformance"];
+
+% commandList = ["load kevin_cad"];
 % commandList = ["load kevin_cad","geomInfo", "inspect", "printCostBreakdown"];
 % commandList = ["load f18_superhornet", "geomInfo", "INSPECT", "setCond", "0", "2", "1", "1", "1", "printData", "GRAPHING", "geomView"];
 % commandList = ["load f18_superhornet", "geomInfo", "INSPECT", "setCond", "10000", "2", "1", "1", "1", "printData", "q"];

8e6ba177c7fd66e3e315f6fd7fd1c7bba19060b7
jfiler0 <jfiler@vt.edu>
2026-04-05 23:11:32 -0400

    weird stuff in max performance + adding in F414

--- SizingCodeV2/Examples/Plots/max_performance_plots.m
+++ SizingCodeV2/Examples/Plots/max_performance_plots.m
@@ -1,8 +1,5 @@
 function max_performance_plots(perf, N)
 
-    compute_max_mach(perf, 1)
-    perf.model.geom.prop.T0_AB
-
     settings = readSettings();
     perf.model.clear_mem(); perf.clear_data();
     W = 1;
@@ -18,22 +15,22 @@ function max_performance_plots(perf, N)
     if settings.be_imperial
         ydata = m2ft(H);
         ylabel = "Altitude [ft]";
-        % general_contour("Mach Number", ylabel, "TSFC [lbm/lbfhr]", "TSFC", M, ydata, kgNs_2_lbmlbfhr(perf.TSFC), filter)
+        general_contour("Mach Number", ylabel, "TSFC [lbm/lbfhr]", "TSFC", M, ydata, kgNs_2_lbmlbfhr(perf.TSFC), filter)
         general_contour("Mach Number", ylabel, "TA [lb]", "Max Thrust Available", M, ydata, N2lb(perf.TA), filter)
-        % general_contour("Mach Number", ylabel, "Excess Power [ft/s]", "Excess Power", M, ydata, m2ft(perf.ExcessPower), filter)
+        general_contour("Mach Number", ylabel, "Excess Power [ft/s]", "Excess Power", M, ydata, m2ft(perf.ExcessPower), filter)
     else
         ydata = H;
         ylabel = "Altitude [m]";
-        % general_contour("Mach Number", ylabel, "TSFC [s]", "TSFC at T = 1", M, ydata, perf.TSFC, filter)
+        general_contour("Mach Number", ylabel, "TSFC [s]", "TSFC at T = 1", M, ydata, perf.TSFC, filter)
         general_contour("Mach Number", ylabel, "TA [kN]", "Max Thrust Available", M, ydata, perf.TA / 1000, filter)
-        % general_contour("Mach Number", ylabel, "Excess Power [m/s]", "Excess Power", M, ydata, perf.ExcessPower, filter)
+        general_contour("Mach Number", ylabel, "Excess Power [m/s]", "Excess Power", M, ydata, perf.ExcessPower, filter)
     end
-    % general_contour("Mach Number", ylabel, "Climb Angle [deg]", "Max Sustained Climb Angle", M, ydata, perf.ClimbAngle, filter)
-    % general_contour("Mach Number", ylabel, "Acc [G]", "Max Axial Accelleration", M, ydata, perf.AxialAccelleration, filter)
-    % % Now need to find the max turn rate
-    % perf.model.clear_mem(); perf.clear_data();
-    % perf.model.cond = generateCondition(perf.model.geom, h_vec_long, M_vec_long, perf.model.geom.input.g_limit.v * one_vec, W * one_vec, one_vec);
-    % % general_contour("Mach Number", "Altitude [m]", "Rate [deg/s]", "Max Level Turn Rate", M, H, perf.LevelTurnRate)
-    % general_contour("Mach Number", ylabel, "Rate [deg/s]", "Max Turn Rate", M, ydata, perf.TurnRate, filter)
+    general_contour("Mach Number", ylabel, "Climb Angle [deg]", "Max Sustained Climb Angle", M, ydata, perf.ClimbAngle, filter)
+    general_contour("Mach Number", ylabel, "Acc [G]", "Max Axial Accelleration", M, ydata, perf.AxialAccelleration, filter)
+    % Now need to find the max turn rate
+    perf.model.clear_mem(); perf.clear_data();
+    perf.model.cond = generateCondition(perf.model.geom, h_vec_long, M_vec_long, perf.model.geom.input.g_limit.v * one_vec, W * one_vec, one_vec);
+    % general_contour("Mach Number", "Altitude [m]", "Rate [deg/s]", "Max Level Turn Rate", M, H, perf.LevelTurnRate)
+    general_contour("Mach Number", ylabel, "Rate [deg/s]", "Max Turn Rate", M, ydata, perf.TurnRate, filter)
     perf.model.clear_mem(); perf.clear_data();
 end
--- SizingCodeV2/Examples/ValidationWork/superhornet_validation.m
+++ SizingCodeV2/Examples/ValidationWork/superhornet_validation.m
@@ -1,7 +1,5 @@
-% initialize
-% matlabSetup
-
-clear; clc;
+initialize
+matlabSetup
 
 build_f18_template
 
@@ -9,35 +7,35 @@ settings = readSettings();
 
 geom = loadAircraft("f18_superhornet", settings);
 
-% model = model_class(settings, geom);
-% perf = performance_class(model);
-% 
-% fprintf("F18 listed empty weight: ~32000f lb. Code value: %.0f\n", N2lb(geom.weights.empty.v))
-% fprintf("F18 listed mtow weight: ~50000f lb. Code value: %.0f\n", N2lb(geom.weights.mtow.v))
-% 
-% v_land_mtow = compute_landing_speed(perf, 1);
-% v_land_empty = compute_landing_speed(perf, 0);
-% 
-% fprintf("Typical F18 landing speed as around 120kt. Our code predicts %.1f - %.1f kt (empty-mtow)\n", ms2kt(v_land_empty), ms2kt(v_land_mtow))
-% 
-% sea_level_dash_mach_mtow = compute_max_mach_at_h(perf, 1, 0);
-% sea_level_dash_mach_empty = compute_max_mach_at_h(perf, 0, 0);
-% 
-% fprintf("Typical F18 sea level dash: 1.03. Our code predicts %.3f - %.3f(empty-mtow)\n", sea_level_dash_mach_empty, sea_level_dash_mach_mtow)
-% 
-% [~, max_mach_mtow] = compute_max_mach(perf, 1);
-% [~, max_mach_empty] = compute_max_mach(perf, 0);
-% 
-% fprintf("Typical F18 max mach: 1.65. Our code predicts %.3f - %.3f(empty-mtow)\n", max_mach_empty, max_mach_mtow)
-% fprintf( "Found approximate F18 range: ~ nm. Estimate of best total range: %.0f nm\n", m2nm(estimate_max_range(perf, 1)) )
-% 
-% W = 0; % not actually getting max performance
-% perf.model.clear_mem(); perf.clear_data(); perf.model.cond = generateCondition(geom, 0, 0.5, 1, W, 0.9); 
-% fprintf("Max Rate of Climb Sealevel (mil) (M0.5) (empty): 12fpm. Code: %.2f\n", m2ft(perf.ExcessPower)*60/1000);
-% 
-% perf.model.clear_mem(); perf.clear_data(); perf.model.cond = generateCondition(geom, 0, 0.5, 1, W, 1);
-% fprintf("Max Rate of Climb Sealevel (AB) (M0.5) (empty): 44fpm. Code: %.2f\n", m2ft(perf.ExcessPower)*60/1000);
+model = model_class(settings, geom);
+perf = performance_class(model);
+
+fprintf("F18 listed empty weight: ~32000f lb. Code value: %.0f\n", N2lb(geom.weights.empty.v))
+fprintf("F18 listed mtow weight: ~50000f lb. Code value: %.0f\n", N2lb(geom.weights.mtow.v))
+
+v_land_mtow = compute_landing_speed(perf, 1);
+v_land_empty = compute_landing_speed(perf, 0);
+
+fprintf("Typical F18 landing speed as around 120kt. Our code predicts %.1f - %.1f kt (empty-mtow)\n", ms2kt(v_land_empty), ms2kt(v_land_mtow))
+
+sea_level_dash_mach_mtow = compute_max_mach_at_h(perf, 1, 0);
+sea_level_dash_mach_empty = compute_max_mach_at_h(perf, 0, 0);
+
+fprintf("Typical F18 sea level dash: 1.03. Our code predicts %.3f - %.3f(empty-mtow)\n", sea_level_dash_mach_empty, sea_level_dash_mach_mtow)
+
+[~, max_mach_mtow] = compute_max_mach(perf, 1);
+[~, max_mach_empty] = compute_max_mach(perf, 0);
+
+fprintf("Typical F18 max mach: 1.65. Our code predicts %.3f - %.3f(empty-mtow)\n", max_mach_empty, max_mach_mtow)
+fprintf( "Found approximate F18 range: ~ nm. Estimate of best total range: %.0f nm\n", m2nm(estimate_max_range(perf, 1)) )
+
+W = 0; % not actually getting max performance
+perf.model.clear_mem(); perf.clear_data(); perf.model.cond = generateCondition(geom, 0, 0.5, 1, W, 0.9); 
+fprintf("Max Rate of Climb Sealevel (mil) (M0.5) (empty): 12fpm. Code: %.2f\n", m2ft(perf.ExcessPower)*60/1000);
+
+perf.model.clear_mem(); perf.clear_data(); perf.model.cond = generateCondition(geom, 0, 0.5, 1, W, 1);
+fprintf("Max Rate of Climb Sealevel (AB) (M0.5) (empty): 44fpm. Code: %.2f\n", m2ft(perf.ExcessPower)*60/1000);
 
 % plot_performance(geom, perf, 50)
-
-displayAircraftGeom(geom)
+% max_performance_plots(perf, 50)
+% displayAircraftGeom(geom)

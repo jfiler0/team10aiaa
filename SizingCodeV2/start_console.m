b0618f58e275f14a0610c46eda8a7fce2266725e
Liam McCary <liamm04@vt.edu>
2026-04-17 14:49:24 -0400

    scissor updates

--- SizingCodeV2/start_console.m
+++ SizingCodeV2/start_console.m
@@ -7,13 +7,12 @@ console = console_class();
 
 % cond order: H, MV, N, W, T
 
-<<<<<<< HEAD
 commandList = ["load f18_superhornet"];
 
 % commandList = ["load kevin_cad","INSPECT","GRAPHING", "maxPerformance"];
 
 % commandList = ["load f18_superhornet", "geomInfo", "INSPECT", "setCond", "0", "2", "1", "1", "1", "printData", "GRAPHING", "geomView"];
-=======
+
 commandList = [];
 
 % commandList = ["load f18_superhornet", "INSPECT", "print"];
@@ -25,9 +24,9 @@ commandList = [];
 % commandList = ["load kevin_cad","geomInfo", "inspect", "printCostBreakdown"];
 % commandList = ["load kevin_cad","INSPECT","GRAPHING", "maxPerformance"];
 
->>>>>>> 86a4568c62de9ef02f37b561f3803f4fb1ce2acd
 % commandList = ["load f18_superhornet", "geomInfo", "INSPECT", "setCond", "10000", "2", "1", "1", "1", "printData", "q"];
 
 %commandList = ["load f18_superhornet","INSPECT","GRAPHING", "levelFlightPerformance"];
 
+commandList = ["load Hellstingerv3", "INSPECT","GRAPHING", "geomView"];
 console.start(commandList);
